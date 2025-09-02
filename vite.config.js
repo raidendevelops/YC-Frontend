6479c1f8054466b723ffdc9791f13c4e99b9e7b5
raiden <raidensch0@gmail.com>
2025-09-02 19:34:51 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {},
+    outDir: "dist",
+  },
+  server: {
+    fs: {
+      strict: false,
+    },
+  },
+  // Important for SPA routing on static hosting
+  base: "./",
 });
