5410b89a01e0608c8fcbf0bcd0e3b5d65ff7fe9c
raiden <raidensch0@gmail.com>
2025-09-02 19:31:09 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound"; // <- import the 404 page
 
 export default function App() {
   return (
@@ -10,6 +12,8 @@ export default function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        {/* Catch-all 404 route */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
